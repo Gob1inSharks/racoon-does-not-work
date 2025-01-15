a8700af50830dff612b228f570efa356d2eec590
Goblin Sharks <101782320+Gob1inSharks@users.noreply.github.com>
2025-01-15 08:01:52 +0800

    Update autonmous.cpp

--- src/autonmous.cpp
+++ src/autonmous.cpp
@@ -1,4 +1,13 @@
 #include "main.h"
 
-void autonomous() {}
+void autonomous() {
 
+    int current_auton_selection = 0;
+
+    switch(current_auton_selection){ 
+        case 0:
+            break;
+        case 1:   
+            break;
+    }
+}
